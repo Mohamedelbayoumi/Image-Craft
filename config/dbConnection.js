1872162fceb597e4df086ae4fa404957670dfa73
Mohamedelbayoumi <mohamedalbyoumi822@gmail.com>
2024-05-05 16:39:46 +0300

    Third Commit

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,14 +1,18 @@
 const { Sequelize } = require('sequelize')
+require("dotenv").config()
 
 const sequelize = new Sequelize({
-    database: process.env.DATABASE_NAME || 'image_craft',
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: process.env.DATABASW_PORT || 3000,
-    username: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD || 'mentafi',
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASW_PORT,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
     dialect: 'mysql',
     define: {
         freezeTableName: true
+    },
+    dialectOptions: {
+        connectTimeout: 15000
     }
 })
 
